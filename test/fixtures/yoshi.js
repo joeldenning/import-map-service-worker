9847b69e89665f5b89a0a2fb1e2be3f8689711f9
Joel Denning <joeldenning@gmail.com>
2020-01-24 23:03:55 -0700

    Fix

--- test/fixtures/yoshi.js
+++ test/fixtures/yoshi.js
@@ -1,9 +1,9 @@
 import DonkeyKong from "./donkey-kong.js";
 import Luigi from "//importmap/luigi";
 
-console.log("Yoshi - ", "a sentient dinosaur");
-console.log("DonkeyKong - ", DonkeyKong);
-console.log("Luigi - ", Luigi);
+console.log("Yoshi -", "a sentient dinosaur");
+console.log("DonkeyKong -", DonkeyKong);
+console.log("Luigi -", Luigi);
 
 document.body.appendChild(
   Object.assign(document.createElement("p"), {
